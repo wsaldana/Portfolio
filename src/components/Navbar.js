edf3004c33266fe31f5ac2e906f2194d69c20a81
wsaldana <53311124+wsaldana@users.noreply.github.com>
2021-06-03 04:32:42 -0600

    Functional navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,48 @@
 import React, { Component } from 'react'
 import '../styles/navbar.scss'
+// Icon Buttons
+import IconButton from '@material-ui/core/IconButton';
+import HomeIcon from '@material-ui/icons/Home';
+import PersonIcon from '@material-ui/icons/Person';
+import ComputerIcon from '@material-ui/icons/Computer';
+import PhoneIphoneIcon from '@material-ui/icons/PhoneIphone';
+import EmailIcon from '@material-ui/icons/Email';
+
+const scrollWindow = (index) => { 
+    window.scrollTo(0, window.innerHeight * index)
+}
 
 export default class Navbar extends Component {
     render() {
         return (
             <div className="container" id="navbarContainer">
-                <p>a</p>
+                <ul>
+                    <li>
+                        <IconButton color="primary" aria-label="home" onClick={()=>scrollWindow(0)} >
+                            <HomeIcon />
+                        </IconButton>
+                    </li>
+                    <li>
+                        <IconButton color="primary" aria-label="about" onClick={()=>scrollWindow(1)}>
+                            <PersonIcon />
+                        </IconButton>
+                    </li>
+                    <li>
+                        <IconButton color="primary" aria-label="web" onClick={()=>scrollWindow(2)}>
+                            <ComputerIcon />
+                        </IconButton>
+                    </li>
+                    <li>
+                        <IconButton color="primary" aria-label="app" onClick={()=>scrollWindow(3)}>
+                            <PhoneIphoneIcon />
+                        </IconButton>
+                    </li>
+                    <li>
+                        <IconButton color="primary" aria-label="contact" onClick={()=>scrollWindow(4)}>
+                            <EmailIcon />
+                        </IconButton>
+                    </li>
+                </ul>
             </div>
         )
     }
