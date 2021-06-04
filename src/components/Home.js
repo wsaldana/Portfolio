e1a45e37e25f4479deed69550e7f6a76855dd1fa
wsaldana <53311124+wsaldana@users.noreply.github.com>
2021-06-03 19:10:05 -0600

    Home title

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from 'react';
+import '../styles/home.scss';
+import '../styles/sectionComponent.scss';
 
 export default function Home() {
     return (
-        <div className="container">
-            <h1>Hello</h1>
+        <div className="container d-flex flex-column justify-content-center" id="titleContainer">
+            <div className='row align-items-end'>
+                <div className='col' id='hi'><h4>Hi! I'm</h4></div>
+                <div className='col-md'>
+                    <div className='row'>
+                        <div className='col-12'><h1>WALTER</h1></div>
+                        <div className='col-12'><h1>SALDANA</h1></div>
+                    </div>
+                </div>
+                <div className='col-12'><h3>WELCOME TO MY PORTFOLIO</h3></div>
+            </div>
         </div>
     )
 }
--- webpack.config.js
+++ webpack.config.js
@@ -39,6 +39,17 @@ module.exports = {
             loader: 'file-loader',
           },
         ],
+      },{
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'fonts/'
+            }
+          },
+        ],
       },
     ],
   },
